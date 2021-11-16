9933144d8eb68fc408bdfbe58e6f8014a445da64
Andi <mail@anderl-bauer.de>
2021-11-16 19:30:41 +0100

    Migrate to central workflow files, add stale bot and codeql analysis (#295)
    * Migrate to central workflow files, add stale bot and codeql analysis
    
    * Set up `test-coverage` script

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.spec.ts'],
+  coverageReporters: ["lcov"],
+  collectCoverageFrom: [
+    "src/**"
+  ],
 }
