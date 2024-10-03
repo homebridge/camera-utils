aa8c43b971f258c05e910559540487fb22c5a47d
Donavan Becker <beckersmarthome@icloud.com>
2024-10-02 19:46:21 -0500

    update dependencies
    update dependencies
    
    update dependencies

--- src/ffmpeg.ts
+++ src/ffmpeg.ts
@@ -1,4 +1,4 @@
-import execa from 'execa'
+import { execa } from 'execa'
 import ffmpegForHomebridgePath from 'ffmpeg-for-homebridge'
 
 export const defaultFfmpegPath: string = ffmpegForHomebridgePath || 'ffmpeg'
@@ -6,9 +6,13 @@ export const defaultFfmpegPath: string = ffmpegForHomebridgePath || 'ffmpeg'
 export async function doesFfmpegSupportCodec(
   codec: string,
   ffmpegPath = defaultFfmpegPath,
-) {
-  const output = await execa(ffmpegPath, ['-codecs'])
-  return output.stdout.includes(codec)
+): Promise<boolean> {
+  try {
+    const { stdout } = await execa(ffmpegPath, ['-codecs'])
+    return stdout.includes(codec)
+  } catch (error) {
+    throw new Error(`FFmpeg not found at path: ${ffmpegPath}, error: ${error}`)
+  }
 }
 
 export async function isFfmpegInstalled(ffmpegPath = defaultFfmpegPath) {
