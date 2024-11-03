0af8b3023014e245338a5bf57db618596a4cf474
Donavan Becker <beckersmarthome@icloud.com>
2024-11-03 13:25:55 -0600

    update tests

--- test/ffmpeg.spec.ts
+++ test/ffmpeg.spec.ts
@@ -1,31 +1,31 @@
 // test/ffmpeg.spec.ts
-import { describe, it, expect } from 'vitest';
-import { doesFfmpegSupportCodec, isFfmpegInstalled } from '../src/index.js';
+import { describe, expect, it } from 'vitest'
+import { doesFfmpegSupportCodec, isFfmpegInstalled } from '../src/index.js'
 
 describe('ffmpeg Utils', () => {
   describe('isFfmpegInstalled', () => {
     it('should return true using default path from ffmpeg-for-homebridge', async () => {
-      expect(await isFfmpegInstalled()).toEqual(true);
-    });
+      expect(await isFfmpegInstalled()).toEqual(true)
+    })
 
     it('should return false if given a bad path to ffmpeg', async () => {
-      expect(await isFfmpegInstalled('some_bad_path')).toEqual(false);
-    });
-  });
+      expect(await isFfmpegInstalled('some_bad_path')).toEqual(false)
+    })
+  })
 
   describe('doesFfmpegSupportCodec', () => {
     it('should return true for a supported codec', async () => {
-      expect(await doesFfmpegSupportCodec('opus')).toEqual(true);
-    });
+      expect(await doesFfmpegSupportCodec('opus')).toEqual(true)
+    })
 
     it('should return false for an unsupported codec', async () => {
-      expect(await doesFfmpegSupportCodec('unknown_codec')).toEqual(false);
-    });
+      expect(await doesFfmpegSupportCodec('unknown_codec')).toEqual(false)
+    })
 
     it('should throw if given a bad path to ffmpeg', async () => {
       await expect(
         doesFfmpegSupportCodec('opus', 'bad_ffmpeg_path'),
-      ).rejects.toThrowError('FFmpeg not found at path: bad_ffmpeg_path');
-    });
-  });
-});
+      ).rejects.toThrowError('FFmpeg not found at path: bad_ffmpeg_path')
+    })
+  })
+})
